20d0213ee363bd43923f2bc6a8cc0853860c921e
Daniel Lahti <daniel.lahti85@gmail.com>
2025-05-19 02:10:21 +0200

    Remove unnecessary test file RepositoryConfigTest.java

--- src/test/java/com/cubeia/wallet_focused/config/AppConfigTest.java
+++ src/test/java/com/cubeia/wallet_focused/config/AppConfigTest.java
@@ -3,22 +3,29 @@ package com.cubeia.wallet_focused.config;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.cubeia.wallet_focused.model.InMemoryWalletRepository;
 import com.cubeia.wallet_focused.model.WalletRepository;
 
-public class AppConfigTest {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class RepositoryConfigTest {
 
+    @Autowired
+    private ApplicationContext context;
+    
     @Test
-    public void testWalletRepositoryBeanCreation() {
-        // Create the config manually
-        AppConfig appConfig = new AppConfig();
-        
-        // Act
-        WalletRepository repositoryFromConfig = appConfig.walletRepository();
+    public void testWalletRepositoryBeanExistsInContext() {
+        // Verify that the WalletRepository bean exists and is an instance of InMemoryWalletRepository
+        WalletRepository repository = context.getBean(WalletRepository.class);
         
         // Assert
-        assertNotNull(repositoryFromConfig);
-        assertTrue(repositoryFromConfig instanceof InMemoryWalletRepository);
+        assertNotNull(repository);
+        assertTrue(repository instanceof InMemoryWalletRepository);
     }
 } 
